fa127185090a80adc68d9deaab3e49e5e8a7075c
JRGCast <joao.ralph1@gmail.com>
2021-10-30 10:43:12 -0300

    colocadas cores primarias e secundarias no tema

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import { makeStyles, ThemeProvider } from '@material-ui/core';
-import { orange } from '@material-ui/core/colors';
-import { createTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/core';
 import './App.css';
 import Routes from './Routes/Routes';
+import theme from './styles/themeStyles';
 
 const App = () => {
-  const theme = createTheme({
-    pallete: {
-      primary: {
-        main: '#f3d2c1'
-      },
-      secondary: {
-        main: '#8bd3dd'
-      },
-      error: {
-        main: orange[500]
-      }
-    },
-  });
   return (
     <ThemeProvider theme={ theme }>
-      <div className="App">
+      <div>
         <p>Uma aplicação web que ajude o usuário a calcular a quantidade de tinta necessária para pintar uma sala.</p>
         <p>Essa aplicação deve considerar que a sala é composta de 4 paredes e deve permitir que o usuário escolha qual a medida de cada parede e quantas janelas e portas possuem cada parede.</p>
         <p>Com base na quantidade necessária o sistema deve apontar tamanhos de lata de tinta que o usuário deve comprar.</p>
--- src/components/WallsInputs.js
+++ src/components/WallsInputs.js
@@ -1,26 +1,28 @@
 import { InputLabel, TextField } from '@material-ui/core';
 import './WallsInputs.css';
-import useStyles from './WallsInputStyles';
+import useStyles from '../styles/WallsInputStyles';
 
 const WallsInputs = ({ quantityOfWalls }) => {
-  const classes = useStyles();
   const amountOfWalls = Array(quantityOfWalls).fill(null);
   const minWallMeasures = 1;
   const maxWallMeasures = 15;
+  const inputPropsWalls = {
+    inputProps: {
+      min: minWallMeasures,
+      max: maxWallMeasures,
+      step: 0.01
+    }
+  };
+  const inputPropsWnD = { min: 0 };
+  const classes = useStyles();
   const theInputs = amountOfWalls.map((_wallAmount, index) => {
     return (
-      <div key={ `content-wrapper-${index}` } className='WallsInputs-content-wrapper'>
+      <div key={ `content-wrapper-${index}` } className={ classes.contentWrapper }>
         <div className='WallsInputs-wrapper WallsInputs-height'>
           <InputLabel htmlFor={ `wall-height-${index + 1}` }>Altura Parede { index + 1 }
             <TextField id={ `wall-height-${index + 1}` }
               type='number'
-              InputProps={ {
-                inputProps: {
-                  min: minWallMeasures,
-                  max: maxWallMeasures,
-                  step: 0.01
-                }
-              } }
+              InputProps={ inputPropsWalls }
               autoFocus={ true }
               required /> metros
           </InputLabel>
@@ -29,24 +31,23 @@ const WallsInputs = ({ quantityOfWalls }) => {
           <InputLabel htmlFor={ `wall-length-${index + 1}` }>Comprimento Parede { index + 1 }
             <TextField id={ `wall-length-${index + 1}` }
               type='number'
-              InputProps={ {
-                inputProps: {
-                  min: minWallMeasures,
-                  max: maxWallMeasures,
-                  step: 0.01
-                }
-              } }
+              InputProps={ inputPropsWalls }
               required /> metros
           </InputLabel>
         </div>
         <div className='WallsInputs-wrapper WallsInputs-window'>
           <InputLabel htmlFor={ `wall-${index + 1}-has-window` }>Possui janela (medidas 2,00m (L) x 1,20m (A))
-            <TextField id={ `wall-${index + 1}-has-window` } type='number' min={ 0 } />
+            <TextField id={ `wall-${index + 1}-has-window` }
+              type='number'
+              InputProps={ inputPropsWnD }
+            />
           </InputLabel>
         </div>
         <div className='WallsInputs-wrapper WallsInputs-door'>
           <InputLabel htmlFor={ `wall-${index + 1}-has-door` }>Possui porta (medidas 0,80m (L) x 1,90m (A))
-            <TextField id={ `wall-${index + 1}-has-door` } type='number' min={ 0 } />
+            <TextField id={ `wall-${index + 1}-has-door` }
+              type='number'
+              InputProps={ inputPropsWnD } />
           </InputLabel>
         </div>
       </div>
