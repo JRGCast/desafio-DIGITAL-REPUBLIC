7204099f538daf9fd4bbf241d09f49be96def03d
JRGCast <joao.ralph1@gmail.com>
2021-10-29 23:58:50 -0300

    apagada logicas relacionadas aos checkbox

--- src/components/WallsInputs.js
+++ src/components/WallsInputs.js
@@ -1,13 +1,10 @@
-import { useState } from "react";
 import './WallsInputs.css';
 
 const WallsInputs = ({ quantityOfWalls }) => {
   const amountOfWalls = Array(quantityOfWalls).fill(null);
   const minWallMeasures = 1;
   const maxWallMeasures = 15;
-  const [doorChecked, setDoorChecked] = useState(false);
   const theInputs = amountOfWalls.map((_wallNumber, index) => {
-    const handleCheck = (e) => console.log(e.target.checked);
     return (
       <div className='WallsInputs-content-wrapper'>
         <div className='WallsInputs-wrapper WallsInputs-height'>
@@ -32,23 +29,20 @@ const WallsInputs = ({ quantityOfWalls }) => {
         </div>
         <div className='WallsInputs-wrapper WallsInputs-window'>
           <label htmlFor={ `wall-${index + 1}-has-window` }>Possui janela (medidas 2,00m (L) x 1,20m (A))
-            <input id={ `wall-${index + 1}-has-window` } type='number' min={ 0 } onChange={ handleCheck } />
+            <input id={ `wall-${index + 1}-has-window` } type='number' min={ 0 } />
           </label>
         </div>
         <div className='WallsInputs-wrapper WallsInputs-door'>
           <label htmlFor={ `wall-${index + 1}-has-door` }>Possui porta (medidas 0,80m (L) x 1,90m (A))
-            <input id={ `wall-${index + 1}-has-door` } type='number' min={ 0 } onChange={ handleCheck } />
+            <input id={ `wall-${index + 1}-has-door` } type='number' min={ 0 } />
           </label>
         </div>
-        { console.log(amountOfWalls) }
       </div>
     );
   });
   return (
     <section className='WallsInputs-main-wrapper'>
       { theInputs }
-      {/* { document.getElementById(`wall-${2}-has-door`).checked ? <h1>Ou</h1> : '' }
-      { document.getElementById(`wall-${3}-has-window`).checked ? <h1>Ou</h1> : '' } */}
     </section>);
 };
 
