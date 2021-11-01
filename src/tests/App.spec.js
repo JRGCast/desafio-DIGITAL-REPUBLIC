3bbe0048271ca16ad8b2b419f42df53acad234a9
JRGCast <joao.ralph1@gmail.com>
2021-11-01 13:36:06 -0300

    AppInside App testado

--- src/tests/App.spec.js
+++ src/tests/App.spec.js
@@ -3,10 +3,23 @@ import { render, screen } from '@testing-library/react';
 import React from 'react';
 import App from '../App';
 
-describe('1 - Test basic text from components', () => {
-  it('Verify if AppInside is rendered inside App', () => {
+describe('1 - Test basic texts from components are rendered inside App', () => {
+  beforeEach(() => {
     render(<App />);
-    const headerElement = screen.getByRole('heading', { level: 1 });
-    expect(headerElement).toBeInTheDocument();
+  });
+  it('Verify if App contains at least 1 header', () => {
+    const allHeaders = screen.getAllByRole('heading');
+    expect(allHeaders.length).toBeGreaterThan(0);
+    console.log(allHeaders);
+  });
+  it('Verify if AppInside text is rendered inside App', () => {
+    const h1LojaTintas = screen.getByRole('heading',
+      {
+        level: 1,
+        name: /LOJA DE TINTAS/
+      });
+    const strPar1 = /Uma aplicação web que ajude o usuário a calcular a quantidade de tinta necessária para pintar uma sala./;
+    const par1 = screen.getByText(strPar1);
+    expect(h1LojaTintas && par1).toBeInTheDocument();
   });
 });
