15eeef41769bca0b3994e123a907d58e794ade03
JRGCast <joao.ralph1@gmail.com>
2021-11-01 13:53:29 -0300

    adicionado lazy e suspense

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import { ThemeProvider } from '@material-ui/core';
+import { LinearProgress, ThemeProvider } from '@material-ui/core';
+import { lazy, Suspense } from 'react';
 import AppInside from './components/AppInside';
-import Routes from './Routes/Routes';
+// import Routes from './Routes/Routes';
 import theme from './styles/themeStyles';
-// const Routes = lazy(() => import('./Routes/Routes'));
+const Routes = lazy(() => import('./Routes/Routes'));
 
 const App = () => {
   return (
-    <ThemeProvider theme={ theme }>
-      <div style={
-        {
-          background: theme.palette.background.default,
-          height: '100%',
-          width: '100%'
-        }
-      }>
-        <AppInside />
-        <Routes />
-      </div>
-    </ThemeProvider >
+    <Suspense fallback={ <LinearProgress /> }>
+      <ThemeProvider theme={ theme }>
+        <div style={
+          {
+            background: theme.palette.background.default,
+            height: '100%',
+            width: '100%'
+          }
+        }>
+          <AppInside />
+          <Routes />
+        </div>
+      </ThemeProvider >
+    </Suspense>
   );
 };
 
